e4e23952007efca297ee14b9fa894d4bfa1733cd
SheepTAOCN <879085709@qq.com>
2021-05-22 23:28:06 +0800

    解决数组传指针计算数组大小的低级错误

--- chapter7/SearchSeq.cpp
+++ chapter7/SearchSeq.cpp
@@ -13,38 +13,38 @@ using ElemType = int;
 
 typedef struct {
     ElemType *data;
-    int tableLen;
+    size_t tableLen;
 }SSTable;
 
 void InitSSTable(SSTable&, ElemType*);                                  // 初始化顺序表
 void DisplaySSTable(const SSTable&);                                    // 打印顺序表 
-size_t SearchSeq(const SSTable&, const ElemType);                       // 顺序查找元素
+size_t SearchSeq(const SSTable&, const ElemType, size_t);                       // 顺序查找元素
 void Clear(SSTable&);                                                   // 回收工作            
 
 int main()
 {
-    int data[] = {15, 36, 78, 45, 12, 46, 82, 64, 19, 35, 28, 67};
+    ElemType data[] = {15, 36, 78, 45, 12, 46, 82, 64, 19, 35, 28, 67};
     SSTable ss;
 
-    InitSSTable(ss, data);
+    InitSSTable(ss, data, sizeof(data)/sizeof(data[0]));
     DisplaySSTable(ss);     cout << endl;
 
     Clear(ss);
     return 0;
 }
 
-void InitSSTable(SSTable& table, ElemType* data) {
-    size_t size = sizeof(data)/sizeof(data[0]) + 1;
+void InitSSTable(SSTable& table, ElemType* data, size_t size) {
+    size = size + 1;
 
     table.data = new ElemType[size];
     for (size_t i = 1; i < size; ++i) {
-        table.data[i] = data[i];
+        table.data[i] = data[i-1];
     }
     table.tableLen = size;
 }
 
 void DisplaySSTable(const SSTable& table) {
-    for (size_t i = 0; i < table.tableLen; ++i) {
+    for (size_t i = 1; i < table.tableLen; ++i) {
         cout << table.data[i] << '\t';
     }
 }
