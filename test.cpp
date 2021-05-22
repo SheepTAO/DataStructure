c6739397c7a055b76462dc83230f77b0daacf2dc
SheepTAOCN <879085709@qq.com>
2021-05-22 12:25:38 +0800

    测试

--- test.cpp
+++ test.cpp
@@ -2,15 +2,12 @@
 // Created by archtao on 2021/5/10.
 //
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int main()
 {
-    int arr[]{1, 2, 3, 4, 5};
-
-    for (int i: arr) {
-        cout << "#" << i-1 << "::" << arr[i-1] << endl;
-    }
+    
 
     return 0;
 }
